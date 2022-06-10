a38ca7e161f0900f8237356cecd96fb538bfd2b7
najlaelam <107081953+najlaelam@users.noreply.github.com>
2022-06-10 15:33:29 +0200

    Add files via upload

---
+++ cn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace New_Appli
+{
+    class cn
+    {
+        public static SqlConnection cnx = new SqlConnection(@"Data Source=DESKTOP-3A96GRL\SQLEXPRESS;Initial Catalog=de;Integrated Security=True");
+        public static void ouvrircnx()
+        {
+            if (cnx.State == ConnectionState.Closed)
+            {
+                cnx.Open();
+            }
+
+
+
+
+
+        }
+        public static void fermercnx()
+        {
+            if (cnx.State == ConnectionState.Open)
+            {
+                cnx.Close();
+            }
+        }
+    }
+}
